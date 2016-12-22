8a8cc67c94243a712632d2eb471a8a8c1c2ca9bf
Benjamin Grant <benjamin.grant@wesleycollege.net>
2016-12-22 18:56:59 +1100

    Added user signup function

--- api_v1.php
+++ api_v1.php
@@ -82,6 +82,7 @@ if ($action == 'award_scheme') {
 	if ($method == 'GET') {
 		//Return user data
 		if (count($request) == 2) {
+			if ($request[1] == 'new') error(405, "Use POST to add a new user");
 			//Get the user id
 			$userid = preg_replace('/[^0-9]+/', '', $request[1]);
 
@@ -118,14 +119,58 @@ if ($action == 'award_scheme') {
 		if (count($request) == 2) {
 			//Make sure that the second keyword is 'new'
 			if (preg_replace('/[^a-z]+/', '', $request[1]) != 'new') {
-				error(400);
+				error(400); //Bad request
+			}
+			//Make sure post is not empty
+			if (empty($_POST)) {
+				error(400); //Bad request
 			}
 
-			//eck
-			error(501);
+			$username = $_POST['user'];
+			$email = $_POST['email'];
+			$raw_password = $_POST['pass'];
+			$raw_password_repeat = $_POST['passRepeat'];
+			
+			//Validate username
+			if (!preg_match('/^[A-Za-z0-9-_]{3,45}$/', $username)) {
+				error(400, "Error: Username must be longer than 2 characters and shorter than 46 characters, and must only contain letters, numbers, hyphens and underscores.");
+			} else {
+				//Check passwords match
+				if ($raw_password != $raw_password_repeat) {
+					error(400, "Error: Passwords must match.");
+				} else {
+					//Vaidate password
+					if (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,72}$/', $raw_password)) {
+						error(400, "Error: Password must be longer than 7 characters, and must only contain at least one lowercase letter, one uppercase letter and one number.");
+					} else {
+						//Validate email
+						if (filter_var(strtolower(trim($email)), FILTER_VALIDATE_EMAIL) === false) {
+							error(400, "Error: Email address is not valid.");
+						} else {
+							$result = mysqli_query($link, "SELECT username FROM users WHERE UPPER(username)=UPPER('$username')");
+							//If username exists in db already
+							if ($result && mysqli_num_rows($result) > 0) {
+								mysqli_free_result($result);
+								error(400, "Error: Username is taken.");
+							} else {
+								//Make the new account
+								$password_hash = password_hash($raw_password, PASSWORD_DEFAULT);
+								if (mysqli_query($link, "INSERT INTO users (username, password, email, registerDate, originalIp) VALUES ('$username','$password_hash','" . strtolower(trim($email)) . "',NOW(),'" . $_SERVER['REMOTE_ADDR'] . "')")) {
+									//"Success: New user created successfully.";
+									http_response_code(201);
+
+									die('{"username": ' . $username . ',"password_hash":"' . $password_hash . '"}');
+								} else {
+									error(404, "Error: " . mysqli_error($link));
+								}
+							}
+							mysqli_close($link);
+						}
+					}
+				}
+			}
 
 			mysqli_close($link);
-			
 		} else {
 			error(400); //Bad request
 		}
